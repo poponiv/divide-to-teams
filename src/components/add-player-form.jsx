e5ab821d624434411235f570e3b5ed6ab5888a19
Niv Dadush <poponiv@gmail.com>
2021-12-29 01:43:21 +0200

    change layout a little and add useful buttons

--- src/components/add-player-form.jsx
+++ src/components/add-player-form.jsx
@@ -1,6 +1,11 @@
 import { Component } from "react";
 import { Badge, Button, Card, Col, Container, Form, ListGroup, Row } from "react-bootstrap";
 
+
+function createPlayer(name, level) {
+    return {playerName: name, level};
+}
+
 function initializeTeams() {
     return [
         {players: [], name: "Team 1", totalLevel: 0},
@@ -13,11 +18,27 @@ class AddPlayerForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            numberOfTeams: 3,
+            numberOfPlayersInTeam: 11,
             addedPlayers: [],
             playerName: '',
-            teams: initializeTeams(),
+            teams: [],
             level: 1
         };
+    }
+    
+    addRandomPlayers() {
+        const addedPlayers = [];
+        const iterations = this.state.numberOfTeams * this.state.numberOfPlayersInTeam;
+        for (let i = 0; i < iterations; i++) {
+            addedPlayers.push(createPlayer("Player " + i, Math.ceil(Math.random()*10)));
+        }
+        addedPlayers.sort((a,b) => b.level - a.level);
+        this.setState({addedPlayers})
+    }
+
+    resetPlayers() {
+        this.setState({teams: [], addedPlayers: []});
     }
 
     handleUserInput(e) {
@@ -27,7 +48,8 @@ class AddPlayerForm extends Component {
     }
 
     handleAdd() {
-        const addedPlayers = [...this.state.addedPlayers, {playerName: this.state.playerName, level: parseInt(this.state.level)}]
+        const newPlayer = createPlayer(this.state.playerName, parseInt(this.state.level));
+        const addedPlayers = [...this.state.addedPlayers, newPlayer];
         addedPlayers.sort((a,b) => b.level - a.level);
         this.setState({
             addedPlayers,
@@ -35,6 +57,12 @@ class AddPlayerForm extends Component {
             level: 1
         });
         
+    }
+
+    deletePlayer(playerInd) {
+        const addedPlayers = this.state.addedPlayers.slice();
+        addedPlayers.splice(playerInd, 1);
+        this.setState({addedPlayers})
     }
 
     setTeams() {
@@ -58,9 +86,9 @@ class AddPlayerForm extends Component {
     render() {
         return (
             <Container className="mt-3">
-                <Row>
-                    <Form>
-                        <Form.Group as={Row} className="mb-3">
+                <Form>
+                    <Form.Group as={Row} className="mb-3">
+                        <Row>
                             <Col xs="auto">
                                 <Form.Control 
                                         name="playerName"
@@ -79,47 +107,61 @@ class AddPlayerForm extends Component {
                                             onChange={(e) => this.handleUserInput(e)} />
                             </Col>
                             <Col xs="auto">
-                                <Button onClick={() => this.handleAdd()}>Add</Button>
+                                <Button variant="outline-primary" type="submit" onClick={(e) => {e.preventDefault(); this.handleAdd()}}>Add</Button>
+                            </Col>
+                        </Row>
+                        <Row className="mt-4">
+                            <Col xs="auto">
+                                <Button onClick={() => this.addRandomPlayers()}>Add Random Players</Button>
                             </Col>
                             <Col xs="auto">
-                                <Button variant="danger"  onClick={() => this.setTeams()}>Set Teams</Button>
+                                <Button onClick={() => this.setTeams()}>Set Teams</Button>
                             </Col>
-                        </Form.Group>
-                        
-                    </Form>
-                </Row>
+                            <Col xs="auto">
+                                <Button variant="danger" onClick={() => this.resetPlayers()}>Reset Players</Button>
+                            </Col>
+                        </Row>
+                    </Form.Group>
+                </Form>
 
-                <Row>
-                    <Col xs={4}>
-                        <Card style={{width: '18rem'}}>
+                <Row className="mt-4">
+                    <Col xs={2}>
+                        <Card>
                             <Card.Header>Available Players</Card.Header>
                             <ListGroup>
-                                {this.state.addedPlayers.map((player) =>
-                                        <ListGroup.Item as={"li"}>
-                                            {player.playerName}
-                                            <Badge>{player.level}</Badge>
+                                {this.state.addedPlayers.map((player, playerInd) =>
+                                        <ListGroup.Item as={"li"} className="d-flex justify-content-between">
+                                            <span className="align-items-start">
+                                                {player.playerName}
+                                                <Badge pill>{player.level}</Badge>
+                                            </span>
+                                            <span className="align-items-end">
+                                                <Button as={Badge} bg="secondary" onClick={() => this.deletePlayer(playerInd)}>X</Button>
+                                            </span>
                                         </ListGroup.Item>
                                 )}
                             </ListGroup>
                         </Card>
                     </Col>
-                </Row>
-                <Row className="mt-5">
-                    {this.state.teams.map((team) =>
-                            <Col xs={4}>
-                                <Card  style={{width: '18rem'}}>
-                                    <Card.Header>{team.name}</Card.Header>
-                                    <ListGroup>
-                                    {team.players.map( (player) => 
-                                    <ListGroup.Item>
-                                        {player.playerName}
-                                        <Badge>{player.level}</Badge>
-                                    </ListGroup.Item>) }
-                                    </ListGroup>
-                                    <Card.Footer>Total: {team.totalLevel}</Card.Footer>
-                                </Card>
-                            </Col>
-                        )}
+                    <Col xs={{span: 6, offset: 4}}>
+                        <Row>
+                        {this.state.teams.map((team) =>
+                                <Col xs={"auto"}>
+                                    <Card>
+                                        <Card.Header>{team.name}</Card.Header>
+                                        <ListGroup>
+                                        {team.players.map( (player) => 
+                                        <ListGroup.Item>
+                                            {player.playerName}
+                                            <Badge pill>{player.level}</Badge>
+                                        </ListGroup.Item>) }
+                                        </ListGroup>
+                                        <Card.Footer>Total: {team.totalLevel}</Card.Footer>
+                                    </Card>
+                                </Col>
+                            )}
+                        </Row>
+                    </Col>
                 </Row>
             </Container>
         );
